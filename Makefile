69135fe1cd5a0e01f4c5dc97a8a587a24214689e
Lyndon Maydwell <maydwell@gmail.com>
2012-02-22 16:03:07 +0800

    Rewriting example

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ all:
 
 clean:
 	rm sqlpipe
+
+test: all
+	(echo 1 2 3; echo 4 5 6; echo 7 8 9) | ./sqlpipe "select c1 c2 from data"
--- sqlpipe.c
+++ sqlpipe.c
@@ -3,13 +3,16 @@
 #include <unistd.h> // GetOpt
 
 /*
- * Example: echo "1 2 3\n4 5 6\n7 8 9" | sqlpipe "select c1 c3 from data" "select c2 + c3 from data where c1 + c2 > 5 order by c3 desc"
+ * Example: (echo "1 2 3"; echo "4 5 6"; echo "7 8 9") | sqlpipe "select c1 c3 from data" "select c2 + c3 from data where c1 + c2 > 5 order by c3 desc"
  *
  * Output:  1  3
  *          4  6
+ *          7  9
  *          17
  *          11
  */
+
+const char sep_default = ' ';
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   int i;
@@ -22,28 +25,30 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 
 int main(int argc, char **argv){
 
-  sqlite3 *db;
-  char *zErrMsg = 0;
-  int rc;
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
 
-  if( argc!=3 ){
-    fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
-    return(1);
-  }
+	if( argc == 1 ) {
+		fprintf(stderr, "Usage: %s <sql>*\n", argv[0]);
+		return(1);
+	}
 
-  rc = sqlite3_open(argv[1], &db);
-  if( rc ){
-    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-    sqlite3_close(db);
-    return(1);
-  }
+	char c;
 
-  rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
-  if( rc!=SQLITE_OK ){
-    fprintf(stderr, "SQL error: %s\n", zErrMsg);
-    sqlite3_free(zErrMsg);
-  }
+	while( ( c = getc(stdin) ) >= 0 ) {
+		printf("Char: %c\n", c);
+	}
 
-  sqlite3_close(db);
-  return 0;
+	rc = sqlite3_open(":memory:", &db);
+
+	rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+
+	if( rc!=SQLITE_OK ){
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+
+	sqlite3_close(db);
+	return 0;
 }
