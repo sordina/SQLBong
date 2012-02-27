bddccd08d4734f20911aaa9a8ffbcd0dc8f07429
Lyndon Maydwell <maydwell@gmail.com>
2012-02-27 18:20:49 +0800

    Argument processing

--- options.h
+++ options.h
@@ -1,8 +1,33 @@
 
 #include <stdio.h>
+#include <string.h>
 
-
-struct globalArgs_t {
-	char* database_url;
+typedef struct globalArgs_t {
+	char* file;
 	char* delimiter;
 } globalArgs;
+
+globalArgs getOpts(int* argc, char** argv) {
+
+	int i;
+	int max = 0;
+
+	globalArgs result;
+
+	result.file      = NULL;
+	result.delimiter = NULL;
+
+	for(i = 0; i < (*argc) - 1; i++) {
+		if(       strcmp("-f",argv[i])) {
+			result.file = argv[i+1];
+			max = i;
+
+		} else if(strcmp("-d",argv[i])) {
+			result.delimiter = argv[i+1];
+			max = i;
+		}
+		i += 2;
+	}
+
+	return result;
+}
