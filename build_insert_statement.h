6bc71dba8b76f8570a2856b8adaa0582d83bf5f0
Lyndon Maydwell <maydwell@gmail.com>
2012-02-27 13:34:03 +0800

    Needed to add multiplier for build statement. Seems too long though

--- build_insert_statement.h
+++ build_insert_statement.h
@@ -11,18 +11,18 @@ char* insert(int num) {
 #endif
 
 	// TODO: This is quite fragile be careful if modifying
-	const char* dstmt      = "insert into data()";
-	const char* vstmt      = " values()";
+	const char  dstmt[]    = "insert into data()";
+	const char  vstmt[]    = " values()";
 	const int   dlen       = strlen(dstmt);
 	const int   vlen       = strlen(vstmt);
 	const int   boiler_len = dlen + vlen;
 	const int   dyn_factor = 1 + (int) log10(num);
 	const int   dyn_len    = dyn_factor * num; // represents the size of the column-number text (minus formatting) (overshoots estimate by log(n) factor)
-	char*       dyn_part   = malloc(sizeof(char) * dyn_factor + 3); // constant is 'c' ',' '\0'
-	char*       result     = malloc(sizeof(char) * (boiler_len + (3 * num) + dyn_len) + 2);
+	char*       dyn_part   = malloc(sizeof(char) * (dyn_factor + 3)); // constant is 'c' ',' '\0'
+	char*       result     = malloc(sizeof(char) * (boiler_len + (4 * num) + dyn_len) + 2);
 	char*       p          = result; // movable pointer for printing
 
-	memcpy(p, dstmt, dlen);
+	memcpy(p, dstmt, dlen * sizeof(char));
 	p += dlen - 1;
 
 	int i;
@@ -34,7 +34,7 @@ char* insert(int num) {
 	}
 	p[-1] = ')';
 
-	memcpy(p, vstmt, vlen);
+	memcpy(p, vstmt, vlen * sizeof(char));
 	p += vlen - 1;
 
 	for(i = 0; i < num; i++) {
@@ -47,9 +47,10 @@ char* insert(int num) {
 	p[ 1] = '\0';
 	p[ 2] = '\0';
 	// TODO: Check UTF8 fix
-	//
 	
+#ifdef DEBUG
 	printf("Inside build_insert_statement - Created result [%s].\n", result);
+#endif
 
 	return result;
 }
